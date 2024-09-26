0c7f6db3e6693575b313dfe685ce6722cd108a3d
Binnur KURT <deepcloudlabs@gmail.com>
2024-09-26 11:41:05 +0300

    Update api.js

--- study-developing-middleware-on-nodejs/api.js
+++ study-developing-middleware-on-nodejs/api.js
@@ -5,6 +5,7 @@ const api = express();
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
+const {getAllEmployees, getEmployeeById} = require("./mongo-repository-hr");
 
 const createApi = (callback) => {
     api.use(bodyParser.json({limit: "5mb"}));
@@ -17,6 +18,38 @@ const createApi = (callback) => {
 const getApi = () => {
      return api;
 }
+
+//region Query Methods
+api.get("/hr/api/v1/employees", (req,res) => {
+   const pageNo = Number(req.query.page || 0);
+   const pageSize = Number(req.query.size || 20);
+   getAllEmployees(pageNo,pageSize)
+   .then((employees) => {
+       res.set("Content-Type", "application/json");
+       res.status(200).send(employees);
+   })
+   .catch(error => {
+       res.status(400).send({error});
+   })
+});
+
+api.get("/hr/api/v1/employees/:identity", (req,res) => {
+    const identity = req.params.identity;
+    getEmployeeById(identity)
+        .then((employee) => {
+            res.set("Content-Type", "application/json");
+            res.status(200).send(employee);
+        })
+        .catch(error => {
+            res.status(400).send({error});
+        })
+});
+
+//endregion
+
+//region Command Methods
+
+//endregion
 
 exports.getApi = getApi;
 exports.createApi = createApi;
